3878e887e3219eae0425ce6d8f1a0f664805048d
Tim Clemans <realtimclemans@gmail.com>
2020-08-18 20:32:59 -0700

    fix login issue

--- register/views.py
+++ register/views.py
@@ -10,7 +10,7 @@ def register(response):
         if form.is_valid():
             form.save()
             print('saved')
-            return redirect("/profile")
+            return redirect("/login")
 
     else:
         form = RegisterForm()
